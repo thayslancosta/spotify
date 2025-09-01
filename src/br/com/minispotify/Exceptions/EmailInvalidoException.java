4526c805e1fe05be381200e74ce62728078785c3
thayslancosta <thayslan.costa@gmail.com>
2025-09-01 12:37:08 -0300

    atualizando com exceptions personalidadas

--- src/br/com/minispotify/Exceptions/EmailInvalidoException.java
+++ src/br/com/minispotify/Exceptions/EmailInvalidoException.java
@@ -2,11 +2,6 @@ package br.com.minispotify.Exceptions;
 
 public class EmailInvalidoException extends Exception {
 
-//    public EmailInvalidoException(String email, String mensagem) {
-//        super("Email '" + email + "' inválido!" + mensagem);
-//    }
-
-
     public EmailInvalidoException() {
         super("Email inválido! Digite um email com '@'.");
     }
